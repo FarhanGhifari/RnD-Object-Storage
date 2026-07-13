9d31a48a715b186564bf3f3732ffa0350d98a7d7
Muhammad Farhan Al Ghifari <al.ghifarifarhan2503@gmail.com>
2026-07-13 10:56:10 +0700

    object-storage3

--- src/utils/file-helper.js
+++ src/utils/file-helper.js
@@ -6,6 +6,7 @@ const path = require('path');
  * @param {string} fileName
  * @returns {string}
  */
+
 function sanitizeFileName(fileName) {
   if (typeof fileName !== 'string') return 'file_tanpa_nama.bin';
   return path.basename(fileName).replace(/[^\w.-]/g, '_') || 'file_tanpa_nama.bin';
